eb2c127eb3b079ee7abd8ae245dfaa36ed197624
rayen-heroshima <rayen.hammami@esen.tn>
2024-11-15 21:53:53 +0100

    Describe the changes you made

--- components/program.tsx
+++ components/program.tsx
@@ -5,34 +5,33 @@ import { Timeline } from "@/components/ui/timeline";
 export function EventProgram() {
   const data = [
     {
-      title: "8:30 : Accueil",
+      title: "8:30 : Accueil & Lancement",
       content: (
-        <div className=" dark:bg-neutral-800 p-5  mb-3">
-          <p className="text-white dark:text-neutral-200 text-sm md:text-base font-medium mb-4 leading-relaxed">
-            C'est le début officiel de l'événement avec un accueil chaleureux des participants. L'occasion idéale pour se familiariser avec les objectifs de la journée et rencontrer les premiers intervenants et organisateurs.
-          </p>
+        <div className="dark:bg-neutral-800 p-5 mb-3">
+          
           <div className="grid grid-cols-2 gap-4">
-            <Image
-              src="https://assets.aceternity.com/templates/startup-1.webp"
-              alt="startup template"
-              width={500}
-              height={500}
+            <video
+              controls
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-xl"
-            />
+            >
+              <source src="/planing/vid.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
           </div>
         </div>
       ),
-    },
+    }
+    ,
     {
-      title: "9:00 : Les défis du Digital Business",
+      title: "9:00 : Panel sur les Défis du Business Digital",
       content: (
-        <div className=" dark:bg-neutral-800 p-5  mb-3">
-          <p className="text-white dark:text-neutral-200 text-sm md:text-base font-medium mb-4 leading-relaxed">
-            Une présentation sur les défis actuels auxquels sont confrontées les entreprises dans le domaine du business numérique, avec des discussions sur les stratégies et innovations pour réussir dans ce secteur.
+        <div className="dark:bg-neutral-800 p-5 mb-3">
+          <p className="text-white dark:text-neutral-200 text-lg md:text-xl font-medium mb-4 leading-relaxed">
+            Découvrez les dernières tendances et innovations dans le domaine du digital. Une présentation sur les défis actuels auxquels sont confrontées les entreprises et des stratégies pour réussir dans ce secteur.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/pro/hero-sections.png"
+              src="/planing/digi.jpg"
               alt="hero template"
               width={500}
               height={500}
@@ -43,15 +42,15 @@ export function EventProgram() {
       ),
     },
     {
-      title: "9:45 : Startup'Innov",
+      title: "9:45 : Startup’Innov",
       content: (
-        <div className=" dark:bg-neutral-800 p-5  mb-3">
-          <p className="text-white dark:text-neutral-200 text-sm md:text-base font-medium mb-4 leading-relaxed">
-            Un moment dédié à l'innovation dans la culture des startups et aux solutions novatrices qu'elles apportent dans différents domaines, allant des technologies aux modèles d'affaires disruptifs.
+        <div className="dark:bg-neutral-800 p-5 mb-3">
+          <p className="text-white dark:text-neutral-200 text-lg md:text-xl font-medium mb-4 leading-relaxed">
+            Inspirez-vous des startups émergentes qui façonnent l'avenir avec des solutions innovantes et des modèles d'affaires disruptifs.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/features-section.png"
+              src="/planing/innov.jpg"
               alt="feature template"
               width={500}
               height={500}
@@ -64,13 +63,11 @@ export function EventProgram() {
     {
       title: "10:30 : Pause Café",
       content: (
-        <div className=" dark:bg-neutral-800 p-5  mb-3">
-          <p className="text-white dark:text-neutral-200 text-sm md:text-base font-medium mb-4 leading-relaxed">
-            Un temps de pause pour se détendre, prendre un café et échanger avec d'autres participants. C'est également une excellente occasion de créer des liens et de discuter des sessions précédentes.
-          </p>
+        <div className="dark:bg-neutral-800 p-5 mb-3">
+          
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/features-section.png"
+              src="/planing/cafe.jpg"
               alt="feature template"
               width={500}
               height={500}
@@ -81,15 +78,15 @@ export function EventProgram() {
       ),
     },
     {
-      title: "10:45 : Reso'Débat",
+      title: "10:45 : Reso’Débats",
       content: (
-        <div className=" dark:bg-neutral-800 p-5  mb-3">
-          <p className="text-white dark:text-neutral-200 text-sm md:text-base font-medium mb-4 leading-relaxed">
-            Un forum de discussion où les participants peuvent échanger des idées, poser des questions et partager leurs expériences sur des sujets d'actualité dans le monde du business et de l'innovation.
+        <div className="dark:bg-neutral-800 p-5 mb-3">
+          <p className="text-white dark:text-neutral-200 text-lg md:text-xl font-medium mb-4 leading-relaxed">
+            Participez à des discussions dynamiques sur les enjeux clés de l'industrie et échangez des idées avec des experts et autres participants.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/features-section.png"
+              src="/planing/debat.jpg"
               alt="feature template"
               width={500}
               height={500}
@@ -102,13 +99,13 @@ export function EventProgram() {
     {
       title: "11:45 : Visite des Stands",
       content: (
-        <div className=" dark:bg-neutral-800 p-5  mb-3">
-          <p className="text-white dark:text-neutral-200 text-sm md:text-base font-medium mb-4 leading-relaxed">
-            Explorez les différents stands et expositions des partenaires et entreprises présents à l'événement. C'est l'occasion de découvrir de nouveaux produits, services ou solutions.
+        <div className="dark:bg-neutral-800 p-5 mb-3">
+          <p className="text-white dark:text-neutral-200 text-lg md:text-xl font-medium mb-4 leading-relaxed">
+            Explorez les opportunités proposées par nos entreprises partenaires. Découvrez de nouveaux produits et services tout en créant des contacts professionnels.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/features-section.png"
+              src="/planing/NETWORKING2.png"
               alt="feature template"
               width={500}
               height={500}
@@ -121,13 +118,13 @@ export function EventProgram() {
     {
       title: "13:30 : Atelier Certifié PMI",
       content: (
-        <div className=" dark:bg-neutral-800 p-5  mb-3">
-          <p className="text-white dark:text-neutral-200 text-sm md:text-base font-medium mb-4 leading-relaxed">
-            Participez à un atelier certifié PMI (Project Management Institute) sur la gestion de projets, qui vous fournira des outils et des techniques pour gérer efficacement vos projets professionnels.
+        <div className="dark:bg-neutral-800 p-5 mb-3">
+          <p className="text-white dark:text-neutral-200 text-lg md:text-xl font-medium mb-4 leading-relaxed">
+            Développez vos compétences en gestion de projets avec une session certifiée PMI. Acquérez des outils et techniques pour une gestion efficace de vos projets professionnels.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/features-section.png"
+              src="/planing/ATELIERR-PMI-INJAZ.jpg"
               alt="feature template"
               width={500}
               height={500}
@@ -140,19 +137,10 @@ export function EventProgram() {
     {
       title: "15:30 : Clôture",
       content: (
-        <div className=" dark:bg-neutral-800 p-5  mb-3">
-          <p className="text-white dark:text-neutral-200 text-sm md:text-base font-medium mb-4 leading-relaxed">
-            La session de clôture où des discours de remerciements seront prononcés et l'événement prendra officiellement fin. Un moment pour résumer les points clés de la journée et encourager les participants à appliquer ce qu'ils ont appris.
+        <div className="dark:bg-neutral-800 p-5 mb-3">
+          <p className="text-white dark:text-neutral-200 text-lg md:text-xl font-medium mb-4 leading-relaxed">
+          Merci pour votre présence !!
           </p>
-          <div className="grid grid-cols-2 gap-4">
-            <Image
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-xl"
-            />
-          </div>
         </div>
       ),
     },
